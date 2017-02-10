d9e26a3ab9bd367fe210836d49bce93019c3af5c
Vinita Atre <ingenue.vinita@gmail.com>
2017-02-09 23:04:55 -0500

    added dummy home page

---
+++ app/src/main/java/com/umd/vinita/staar/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.umd.vinita.staar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+}
--- app/src/main/java/com/umd/vinita/staar/LoginActivity.java
+++ app/src/main/java/com/umd/vinita/staar/LoginActivity.java
@@ -3,6 +3,7 @@ package com.umd.vinita.staar;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -328,6 +329,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
               //  finish();
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
