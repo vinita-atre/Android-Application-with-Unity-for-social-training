17fc5c2c128ab784526833bfd504b41764a40ae9
Vinita Atre <ingenue.vinita@gmail.com>
2017-02-17 12:38:01 -0500

    customize nav drawerwith icons; create home page layout

--- app/src/main/java/com/umd/vinita/staar/HomepageActivity.java
+++ app/src/main/java/com/umd/vinita/staar/HomepageActivity.java
@@ -80,19 +80,17 @@ public class HomepageActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_progress) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_module1) {
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_module2) {
 
-        } else if (id == R.id.nav_send) {
-
-        }
+        } 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
