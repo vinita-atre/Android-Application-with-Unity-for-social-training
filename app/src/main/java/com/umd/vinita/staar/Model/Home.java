8024c17b78aae34f2ebb84943b7decfa5029528f
Vinita Atre <ingenue.vinita@gmail.com>
2017-03-01 13:33:42 -0500

    Creadted model for user progress

---
+++ app/src/main/java/com/umd/vinita/staar/Model/Home.java
@@ -0,0 +1,101 @@
+package com.umd.vinita.staar.Model;
+
+/**
+ * Created by Vinita on 2/25/2017.
+ */
+public class Home {
+
+
+
+    String brandName;
+    String thumbnailImageUrl;
+    int productId;
+    String originalPrice;
+    String price;
+    String percentOff;
+    String productUrl;
+    String productName;
+    int styleId;
+    int colorId;
+
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getThumbnailImageUrl() {
+        return thumbnailImageUrl;
+    }
+
+    public void setThumbnailImageUrl(String thumbnailImageUrl) {
+        this.thumbnailImageUrl = thumbnailImageUrl;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(String originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPercentOff() {
+        return percentOff;
+    }
+
+    public void setPercentOff(String percentOff) {
+        this.percentOff = percentOff;
+    }
+
+    public String getProductUrl() {
+        return productUrl;
+    }
+
+    public void setProductUrl(String productUrl) {
+        this.productUrl = productUrl;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getStyleId() {
+        return styleId;
+    }
+
+    public void setStyleId(int styleId) {
+        this.styleId = styleId;
+    }
+
+    public int getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(int colorId) {
+        this.colorId = colorId;
+    }
+}
---
+++ app/src/main/java/com/umd/vinita/staar/Model/UserProgress.java
@@ -0,0 +1,40 @@
+package com.umd.vinita.staar.Model;
+
+/**
+ * Created by Vinita on 2/25/2017.
+ */
+public class UserProgress {
+    String modulesCompleted;
+    String curentModule;
+    int currentModuleID;
+
+    public void setModulesCompleted(String modulesCompleted) {
+        this.modulesCompleted = modulesCompleted;
+    }
+
+    public void setCurentModule(String curentModule) {
+        this.curentModule = curentModule;
+    }
+
+    public void setCurrentModuleID(int currentModuleID) {
+        this.currentModuleID = currentModuleID;
+    }
+
+    public String getModulesCompleted() {
+        return modulesCompleted;
+    }
+
+    public String getCurentModule() {
+        return curentModule;
+    }
+
+    public int getCurrentModuleID() {
+        return currentModuleID;
+    }
+
+
+
+
+
+
+    }
